8f4f4b29bf71ec34e187c52c1a72619ceb4bbda3
DarknMan <72549846+veenayagam@users.noreply.github.com>
2022-04-24 12:43:43 +0800

    Update data_collection.py

--- data_collection.py
+++ data_collection.py
@@ -9,7 +9,7 @@
 import glassdoor_scraper as gs 
 import pandas as pd 
 
-path = "C:/Veenayagam_Things/ML-things/ML Projects/ds_salary_estimator/data_collection/scraping-glassdoor-selenium/chromedriver"
+path = "C:/ds_salary_estimator/data_collection/scraping-glassdoor-selenium/chromedriver"
 
 df = gs.get_jobs('data analyst',500, False, path, 15)
 df.to_csv('glassdoor_data_analyst_jobs.csv', index = False)
